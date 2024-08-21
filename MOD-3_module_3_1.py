d8de8548cb363b5fd1db214276d81fc735161c3f
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-08-21 17:20:33 +0300

    Add files via upload

---
+++ MOD-3_module_3_1.py
@@ -0,0 +1,24 @@
+# Домашняя работа по уроку "Пространство имён"
+
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    global calls
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+def is_contains(string, list_to_search):
+    global calls
+    count_calls()
+    return string in list_to_search
+
+# Вызов функций с произвольными данными
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
